fe376cb3a291e20c3321da7a4cf7945c68f13372
Deepanshi Singhal <deepanshisinghal1523@gmail.com>
2025-10-01 15:46:39 +0530

    made changes in the product and the calculator

--- app/page.tsx
+++ app/page.tsx
@@ -6,26 +6,26 @@ import Testimonial from "../components/homepage/testimonial";
 import Climate from "../components/homepage/climate";
 // import HiddenLayerSection from "@/components/homepage/hiddenLayer";
 import NewsEventsBlogs from "@/components/homepage/newsEventsBlogs";
-import { mockData } from "@/lib/mockData";
+// import { mockData } from "@/lib/mockData";
 import CertificateCarousel from "@/components/homepage/certificates";
 import PartOfSkygreen from "@/components/homepage/partOfSkyGreen/PartOfSkygree";
-import WhySkyGreen from "@/components/homepage/whyShop"
-// import { fetchNewsEventsBlogs } from "@/lib/strapiData";
+import WhySkyGreen from "@/components/homepage/whyShop";
+import { fetchNewsEventsBlogs } from "@/lib/strapiData";
 
 export default async function Home() {
-  // const data = await fetchNewsEventsBlogs();
+  const data = await fetchNewsEventsBlogs();
   return (
     <div>
       <Hero />
       {/* <WhySkygreen/> */}
       <WhyUs />
-      <CertificateCarousel/>
+      <CertificateCarousel />
       <Climate />
       {/* <HiddenLayerSection /> */}
-      <PartOfSkygreen/>
-      <NewsEventsBlogs data={mockData} initialTab="news" autoplayMs={5000} />
+      <PartOfSkygreen />
+      <NewsEventsBlogs data={data} initialTab="news" autoplayMs={5000} />
       <Testimonial />
-      <WhySkyGreen/>
+      <WhySkyGreen />
       {/* <Faq /> */}
     </div>
   );
